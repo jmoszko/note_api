0643c02b4e13855d240655033acd3622c3c2d064
Jaroslaw Moszkowski <jkp437@motorolasolutions.com>
2022-10-31 12:49:18 +0100

    Tests for adding note

--- src/main/java/com/jarmangani/notes/notes_api/user/User.java
+++ src/main/java/com/jarmangani/notes/notes_api/user/User.java
@@ -6,8 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Entity
 @Data
 public class User {
--- src/test/java/com/jarmangani/notes/notes_api/note/NoteControllerTest.java
+++ src/test/java/com/jarmangani/notes/notes_api/note/NoteControllerTest.java
@@ -1,11 +1,74 @@
 package com.jarmangani.notes.notes_api.note;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.jarmangani.notes.notes_api.tag.TagRepository;
+import com.jarmangani.notes.notes_api.user.User;
+import com.jarmangani.notes.notes_api.user.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class NoteControllerTest {
+
+    @Mock private NoteRepository noteRepository;
+    @Mock private UserRepository userRepository;
+    @Mock private TagRepository tagRepository;
+
+    private NoteController noteController;
+    private final String email = "test.user@notes.pl";
+    User user = new User(1, email, "pass");
+
+    @BeforeEach
+    void init() {
+        noteController = new NoteController(noteRepository, userRepository, tagRepository);
+        SecurityContextHolder.setContext(SecurityContextHolder.createEmptyContext());
+        SecurityContextHolder.getContext().setAuthentication(new TestingAuthenticationToken(
+                email, null));
+    }
+
     @Test
-    void testFails() {
+    void createNote_sucess() {
+        NoteInput noteInput = new NoteInput(Collections.singleton("Tag"), "Topic", "Content");
+        when(userRepository.findByEmail(eq(email))).thenReturn(user);
+        when(tagRepository.existsByTag(eq("Tag"))).thenReturn(true);
+
+        noteController.addNote(noteInput);
+
+        ArgumentCaptor<Note> argumentCaptor = ArgumentCaptor.forClass(Note.class);
+        verify(noteRepository).save(argumentCaptor.capture());
+        assertEquals(user, argumentCaptor.getValue().getAuthor());
+        assertEquals(noteInput.getContent(), argumentCaptor.getValue().getContent());
+        assertEquals(noteInput.getTopic(), argumentCaptor.getValue().getTopic());
+        assertEquals(noteInput.getTags(),
+                argumentCaptor.getValue().getTags().stream().map(t -> t.getTag()).collect(Collectors.toSet()));
+    }
+
+    @Test
+    void createNote_failIfMissingTag() {
+        NoteInput noteInput = new NoteInput(Collections.singleton("another-tag"), "Topic", "Content");
+        when(userRepository.findByEmail(eq(email))).thenReturn(user);
+        when(tagRepository.existsByTag(eq("another-tag"))).thenReturn(false);
+
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            noteController.addNote(noteInput);
+        });
+        assertEquals(404, exception.getRawStatusCode());
+
     }
 }
